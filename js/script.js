da1f3aba74f4a840c542f9a294fcf6e2e5eef122
SHA-shwatdubey <krishnadubey0537@gmail.com>
2025-02-01 11:40:40 +0530

    js folder

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// ===== Typing animation =====
+var typed =new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+

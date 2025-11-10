8f73b4e725e2989431dd419d2592d53ae1054140
shashwat <shashwat@duby.com>
2025-11-10 22:56:19 +0530

    Fix folder structure and workflow

--- Portfolio-html-css-js-bootstrap/js/script.js
+++
@@ -1,8 +0,0 @@
-// ===== Typing animation =====
-var typed =new Typed(".typing",{
-    strings:["","Web Designer","Web Developer","Graphic Designer","YouTuber"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
-
